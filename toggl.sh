755e3a4ff3851c405f66a1effe6524edb0a27d25
Albert Bertram <bertrama@umich.edu>
2016-02-19 10:53:20 -0500

    Some custom stuff for invoking python.

--- toggl.sh
+++ toggl.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-PYTHONIOENCODING="utf-8" $(dirname $0)/toggl.py $*
+# First figure out where we're being run from
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+PYTHON_DIR="$(dirname "$DIR")/python"
+
+# This assumes that there's a structure like:
+# .../bin/toggl.sh      <- This file goes here.
+# .../python/pip        <- python/pip dependencies installed here.
+# .../python/toggl-cli  <- https://github.com/mlibrary/toggl-cli
+# ... may be anything.  Maybe /usr/local.  Maybe ~, maybe something else entirely.
+
+PYTHONPATH="$PYTHON_DIR/pip" \
+PYTHONIOENCODING="utf-8" \
+  /usr/bin/python \
+    "$PYTHON_DIR/toggl-cli/toggl.py" \
+    "$@"
+
